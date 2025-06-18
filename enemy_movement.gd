b6a9f7728b7a61a94271149a266045680028b70f
Pete01723 <gamerstagged@yahoo.com>
2025-06-18 21:46:52 +0800

    adjusted enemy_movement apply_friction

--- enemy_movement.gd
+++ enemy_movement.gd
@@ -17,10 +17,10 @@ func Apply_Gravity(delta):
 func Apply_Friction(direction, delta):
 	if direction == 0 and current_character.is_on_floor():
 		current_character.velocity.x = move_toward(current_character.velocity.x, 0.0, movement_values.FRICTION * delta)
-
-func Apply_Air_Resistance(delta):
-	if current_character.velocity.x != 0 and not current_character.is_on_floor():
-		current_character.velocity.x = move_toward(current_character.velocity.x, 0.0, movement_values.AIR_RESISTANCE * delta)
+		nav_agent.velocity.x = current_character.velocity.x
+#func Apply_Air_Resistance(delta):
+#	if current_character.velocity.x != 0 and not current_character.is_on_floor():
+#		current_character.velocity.x = move_toward(current_character.velocity.x, 0.0, movement_values.AIR_RESISTANCE * delta)
 
 func Handle_Acceleration(direction, delta):
 	if not current_character.is_on_floor(): 
@@ -29,8 +29,8 @@ func Handle_Acceleration(direction, delta):
 		current_character.velocity.x = move_toward(current_character.velocity.x, direction * movement_values.SPEED, movement_values.ACCELERATION * delta)		
 		nav_agent.velocity.x = current_character.velocity.x
 		
-func Handle_Air_Acceleration(direction, delta):
-	if current_character.is_on_floor(): 
-		return
-	if direction != 0:
-		current_character.velocity.x = move_toward(current_character.velocity.x, movement_values.SPEED * direction, movement_values.AIR_ACCELERATION * delta)
+#func Handle_Air_Acceleration(direction, delta):
+#	if current_character.is_on_floor(): 
+#		return
+#	if direction != 0:
+#		current_character.velocity.x = move_toward(current_character.velocity.x, movement_values.SPEED * direction, movement_values.AIR_ACCELERATION * delta)
