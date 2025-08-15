36b1c9526ad90ff395258f9923f57576726e49d8
Pete01723 <gamerstagged@yahoo.com>
2025-08-15 12:40:51 +0800

    Updated "enemy_movement.gd" to be more object-oriented

--- Test Enemy/test_enemy.gd
+++ Test Enemy/test_enemy.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
+@export var animated_sprite: AnimatedSprite2D
 var player: Node2D = null
 
 func _physics_process(delta: float) -> void:
-	var next_path_pos = $NavigationAgent2D.get_next_path_position()
-	var direction = position.direction_to(next_path_pos)
-	if player:
-		$NavigationAgent2D.target_position = player.position
-	
+	$EnemyMovementComponent.Handle_Navigation(player)
 	$EnemyMovementComponent.Apply_Gravity(delta)
-	$EnemyMovementComponent.Handle_Acceleration(direction.x, delta)
-	$EnemyMovementComponent.Apply_Friction(direction.x, delta)
+	$EnemyMovementComponent.Handle_Acceleration(delta)
+	$EnemyMovementComponent.Apply_Friction(delta)
+	Handle_Animation()
 	move_and_slide()
-
+	
+func Handle_Animation():
+	if velocity.x > 0:
+		animated_sprite.flip_h = false
+	else:
+		animated_sprite.flip_h = true
+		
 func _on_detection_box_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	player = body
 
