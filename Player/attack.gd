bf2d9f71b04388c3908dbc2f125a16e064059175
AranethWinters <ernestblane.calara-19@cpu.edu.ph>
2025-09-01 02:51:04 +0800

    boss

--- Player/attack.gd
+++ Player/attack.gd
@@ -7,17 +7,18 @@ func _process(delta):
 	Handle_Attack()
 	
 func Handle_Attack():
-	if player.velocity.x < 0 && $HitBox/CollisionShape2D.position.x > 0:
-		$HitBox/CollisionShape2D.position.x *= -1
-	elif player.velocity.x > 0 && $HitBox/CollisionShape2D.position.x < 0:
-		$HitBox/CollisionShape2D.position.x *= -1
+	if player.velocity.x < 0 && $HurtBox/CollisionShape2D.position.x > 0:
+		$HurtBox/CollisionShape2D.position.x *= -1
+	elif player.velocity.x > 0 && $HurtBox/CollisionShape2D.position.x < 0:
+		$HurtBox/CollisionShape2D.position.x *= -1
 	if player.is_attacking == false:
-		$HitBox/CollisionShape2D.disabled = true
+		$HurtBox/CollisionShape2D.disabled = true
 	else:
-		$HitBox/CollisionShape2D.disabled = false
+		$HurtBox/CollisionShape2D.disabled = false
 	
 
-func _on_hit_box_area_entered(area):
+
+func _on_hurt_box_area_entered(area):
 	if area.has_method('damage'):
 		var attack = Attack.new()
 		attack.attack_damage = 1
