53d8d772f0f64edf1cbfd32e506ed5cf1aae8814
Pete01723 <gamerstagged@yahoo.com>
2025-09-15 14:30:55 +0800

    Fixed Editor Bug with Health Component

--- healthBar.gd
+++ healthBar.gd
@@ -8,4 +8,6 @@ func _ready():
 	player.healthChanged.connect(update)
 
 func update():
-	value = (player.get_child(6).health.current_health_points*100)/player.get_child(6).health.max_health_points
+	for i in player.get_child_count():
+		if player.get_child(i).name == "Health":
+			value = (player.get_child(i).health.current_health_points*100)/player.get_child(i).health.max_health_points
